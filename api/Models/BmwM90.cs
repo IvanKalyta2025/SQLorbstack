09fd9ef292afbcf1243e4c0e279b1398e7fd848d
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-02-12 20:03:16 +0100

    Add assembly reference cache for updated NuGet packages in the project

---
+++ api/Models/BmwM90.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class BmwM90
+    {
+        public List<string> Series { get; set; } = new List<string>();
+        public List<int> Years { get; set; } = new List<int>();
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string ManagerForCar { get; set; } = string.Empty;
+
+
+
+    }
+}
